ec90b3566f937dd4d6ab0cea98b3afe7c5a88900
Florian Vogt <florian.vogt@sap.com>
2019-06-17 13:53:53 +0200

    test: fix tree test for getnodecontext

--- test/qunit/model/FHIRTreeBinding.integration.js
+++ test/qunit/model/FHIRTreeBinding.integration.js
@@ -5,29 +5,37 @@ sap.ui.define([
 	"sap/fhir/model/r4/FHIRFilterOperator"
 ], function (TestUtilsIntegration, TestUtils, FHIRFilter, FHIRFilterOperator) {
 	"use strict";
+	
+	var oModel, oTreeBinding;
 
 	function createModel(mParameters) {
-		return TestUtils.createFHIRModel("http://localhost:8080/fhir/R4", mParameters);
+		oModel = TestUtils.createFHIRModel("http://localhost:8080/fhir/R4", mParameters);
 	}
 
+	function createTreeBinding(sPath, oContext, aFilters, mParameters, aSorters){
+		// create binding
+		oTreeBinding = oModel.bindTree(sPath, oContext, aFilters, mParameters, aSorters).initialize();
+	}
+
+
 	QUnit.module("Integration-Tests: FHIRTreeBinding", {
-		before: function () {
-			this.oFhirModel = createModel();
+		beforeEach: function () {
+			createModel();
 		}
 	});
 
 	QUnit.test("check if response has no total property that error in tree binding is thrown", function (assert) {
 		var sPath = "/StructureDefinition";
 		var mParameters = { rootSearch: "base:exact", rootProperty: "baseDefinition", rootValue: "http://hl7.org/fhir/StructureDefinition/DomainResource", nodeProperty: "url", refreshByExpand: "true" };
-		var oTreeBinding = this.oFhirModel.bindTree(sPath, undefined, undefined, mParameters);
-		TestUtilsIntegration.manipulateResponse("http://localhost:8080/fhir/R4/StructureDefinition?base:exact=http://hl7.org/fhir/StructureDefinition/DomainResource&_count=10&_format=json&_total=accurate", this.oFhirModel, TestUtilsIntegration.setTotalUndefined, TestUtilsIntegration.checkErrorMsg.bind(undefined, this.oFhirModel, assert, "FHIR Server error: The \"total\" property is missing in the response for the requested FHIR resource " + sPath));
+		createTreeBinding(sPath, undefined, undefined, mParameters);
+		TestUtilsIntegration.manipulateResponse("http://localhost:8080/fhir/R4/StructureDefinition?base:exact=http://hl7.org/fhir/StructureDefinition/DomainResource&_count=10&_format=json&_total=accurate", oModel, TestUtilsIntegration.setTotalUndefined, TestUtilsIntegration.checkErrorMsg.bind(undefined, oModel, assert, "FHIR Server error: The \"total\" property is missing in the response for the requested FHIR resource " + sPath));
 		oTreeBinding.getContexts();
 	});
 
 	QUnit.test("check that pending request is terminated in treebinding if no entry is found in the response object", function (assert) {
 		var oBaseFilter = new FHIRFilter({ path: "type", operator: FHIRFilterOperator.Contains, value1: "FakeResourceType" });
 		var mParameters = { rootSearch: "base:exact", rootProperty: "baseDefinition", rootValue: "http://hl7.org/fhir/StructureDefinition/DomainResource", nodeProperty: "url", refreshByExpand: "true" };
-		var oTreeBinding = this.oFhirModel.bindTree("StructureDefinition", undefined, [oBaseFilter], mParameters);
+		createTreeBinding("StructureDefinition", undefined, [oBaseFilter], mParameters);
 		var done1 = assert.async();
 		var fnDataReceivedCheck = function () {
 			oTreeBinding.detachDataReceived(fnDataReceivedCheck);
@@ -38,18 +46,28 @@ sap.ui.define([
 		oTreeBinding.getContexts();
 	});
 
-	QUnit.test("Test setSelectedIndex", function(assert){
-		var sPath = "/StructureDefinition";
-		var mParameters = {rootSearch: "base:exact", rootProperty: "baseDefinition", rootValue: "http://hl7.org/fhir/StructureDefinition/DomainResource", nodeProperty: "url"};
-		var oTreeBinding = this.oFhirModel.bindTree(sPath, undefined, undefined, mParameters);
+	QUnit.test("Test getNodeContexts", function(assert){
+		var mParameters = {rootSearch: "base", rootProperty: "baseDefinition", rootValue: "http://hl7.org/fhir/StructureDefinition/DomainResource", nodeProperty: "url"};
+		createTreeBinding("/StructureDefinition", undefined, undefined, mParameters)
+		
 		var done = assert.async();
-		var fnDataReceivedCheck = function () {
-			oTreeBinding.detachDataReceived(fnDataReceivedCheck);
+
+		var fnChangeHandler1 = function () {
+			oTreeBinding.detachChange(fnChangeHandler1);
 			assert.strictEqual(false, oTreeBinding.isInitial(), "Treebinding is not longer in initial state.");
-			// test here the setSelectedIndex method - go to opa tests and debug how nodes are generated at this point in time nodes are not there
+			var aContexts = oTreeBinding.getContexts(0, 10);
+			var oAccountContext = aContexts[0];
+			assert.strictEqual(oModel.getProperty("name", oAccountContext), "Account", "The first context in tree is 'Account'");
+			oTreeBinding.getNodeContexts(oAccountContext, fnChangeHandler2);
+		};
+
+		var fnChangeHandler2 = function(){
+			var aContexts = oTreeBinding.getContexts(0, 10);
+			var oAccountNode = oTreeBinding.getNodeByIndex(0);
+			assert.strictEqual(oAccountNode.nodeState.expanded, false, "The first node is not expanded.");
 			done();
 		};
-		oTreeBinding.attachDataReceived(fnDataReceivedCheck);
-		oTreeBinding.getContexts();
+		oTreeBinding.attachChange(fnChangeHandler1);
+		oTreeBinding.getContexts(0, 10);
 	});
 });
