7956b91585956c17518374fd0c69e4a575351986
Florian Vogt <florian.vogt@sap.com>
2019-07-01 14:23:49 +0200

    add first copy

--- scripts/build/replaceCopyright.js
+++ scripts/build/replaceCopyright.js
@@ -1,11 +1,16 @@
 const patternReplacer = require("./patternReplacer");
+const yaml = require('js-yaml');
+const fs = require('fs');
+const yamlPath = "ui5.yaml";
 
 function getDefaultCopyRight() {
-    return "Arschloch";
+    var content = fs.readFileSync(yamlPath, 'utf8'),
+    var yamlContent = yaml.safeLoad(content);
+    return yamlContent.metadata.copyright;
 }
 
 module.exports = function (copyright = getDefaultCopyRight()) {
-    console.log("OpenUI5-FHIR builder task replaceCopyright...");
+    console.log("OpenUI5-FHIR builder task replaceCopyright started...");
 
     const options = {
         files: ["src/sap/fhir/**/*.js", "src/sap/fhir/.library"],
