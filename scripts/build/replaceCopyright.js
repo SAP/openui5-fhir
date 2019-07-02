5ad6057d4750c2930e6834706e18af0d3200624e
Florian Vogt <florian.vogt@sap.com>
2019-07-02 11:14:39 +0200

    chore: add builder script to prepublishOnly hook

---
+++ scripts/build/replaceCopyright.js
@@ -0,0 +1,26 @@
+const patternReplacer = require("./patternReplacer");
+const yaml = require('js-yaml');
+const fs = require('fs');
+const yamlPath = "ui5.yaml";
+
+function getDefaultCopyRight() {
+    const content = replaceCurrentYear(fs.readFileSync(yamlPath, 'utf8'));
+    const yamlContent = yaml.safeLoad(content);
+    return yamlContent.metadata.copyright;
+}
+
+function replaceCurrentYear(content) {
+    return content.replace(/\$\{currentYear\}/g, new Date().getFullYear());
+}
+
+module.exports = function (copyright = getDefaultCopyRight()) {
+    console.log("OpenUI5-FHIR builder task replaceCopyright started...");
+
+    const options = {
+        files: ["src/sap/fhir/**/*.js", "src/sap/fhir/.library"],
+        from: /\$\{copyright\}/g,
+        to: copyright,
+    };
+
+    return patternReplacer("replaceCopyright", options);
+};
