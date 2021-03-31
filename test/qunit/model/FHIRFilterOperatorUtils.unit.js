e268fe809668b85ee8fcee9e66391bdc49cbf516
AmirthavalliG <amirthavalli.govindarajan@sap.com>
2021-03-31 09:11:07 +0530

    refactor: address review comments

--- test/qunit/model/FHIRFilterOperatorUtils.unit.js
+++ test/qunit/model/FHIRFilterOperatorUtils.unit.js
@@ -1,68 +1,68 @@
 sap.ui.define([
-	"sap/fhir/model/r4/FHIRFilterOperator",
+	"sap/fhir/model/r4/FHIRFilterComplexOperator",
 	"sap/fhir/model/r4/FHIRFilterOperatorUtils",
 	"sap/fhir/model/r4/FHIRFilter"
-], function (FHIRFilterOperator, FHIRFilterOperatorUtils, FHIRFilter) {
+], function (FHIRFilterComplexOperator, FHIRFilterOperatorUtils, FHIRFilter) {
 	"use strict";
 
 	QUnit.module("Unit-Tests: FHIRFilterOperatorUtils", {});
 
 	QUnit.test("Test function getFHIRFilterPrefix ", function (assert) {
-		var oFilter = new FHIRFilter({ operator: FHIRFilterOperator.EQ });
+		var oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.EQ });
 		var sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("eq", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.NE });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.NE });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("ne", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.GE });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.GE });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("ge", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.LE });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.LE });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("le", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.StartsWith });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.StartsWith });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("sw", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.EndsWith });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.EndsWith });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("ew", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.GT });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.GT });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("gt", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.LT });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.LT });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("lt", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.SA });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.SA });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("sa", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.EB });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.EB });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("eb", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.AP });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.AP });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("ap", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.PR });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.PR });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("pr", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.PO });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.PO });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("po", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.IN });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.IN });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("in", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.SS });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.SS });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("ss", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.SB });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.SB });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("sb", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.RE });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.RE });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("re", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.Contains });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.Contains });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("co", sFHIRFilterPrefix);
-		oFilter = new FHIRFilter({ operator: FHIRFilterOperator.NI });
+		oFilter = new FHIRFilter({ operator: FHIRFilterComplexOperator.NI });
 		sFHIRFilterPrefix = FHIRFilterOperatorUtils.getFHIRFilterPrefix(oFilter);
 		assert.strictEqual("ni", sFHIRFilterPrefix);
 	});
