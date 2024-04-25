3670e5ee787ca725dcb0e02f11cb69f8a30e071c
kanikasharma97 <kanikasharma0597@gmail.com>
2024-04-25 12:25:45 +0530

    Update FHIRUtils.js

--- src/sap/fhir/model/r4/FHIRModel.js
+++ src/sap/fhir/model/r4/FHIRModel.js
@@ -814,12 +814,12 @@ sap.ui.define([
 				return mRequestHandles;
 			};
 
-       /**
-        * Retrieves an array of resources that have been removed from the FHIR model.
-        * Iterates through the removed resources,
-        * retrieves corresponding resources from the model, and returns them.
-        * @returns {Array<object>} An array containing the removed resources.
-        */
+	/**
+     * Retrieves an array of resources that have been removed from the FHIR model.
+     * Iterates through the removed resources,
+     * retrieves corresponding resources from the model, and returns them.
+     * @returns {Array<object>} An array containing the removed resources.
+     */
 	FHIRModel.prototype.getRemovedResourcesObject = function () {
 		var resources = [];
 		for (var type in this.mRemovedResources) {
