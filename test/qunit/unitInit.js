33e11237834477fdf273c8c747eab297400c7a2f
Amirtha <amirthavalli.govindarajan@sap.com>
2026-05-26 16:08:45 +0530

    chore(tests): fixes to init

---
+++ test/qunit/unitInit.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/qunit/qunit-2-css",
+	"sap/ui/thirdparty/qunit-2",
+	"sap/ui/qunit/qunit-junit",
+	"sap/ui/qunit/qunit-coverage",
+	"openui5-fhir/test/createMockServer",
+	"openui5-fhir/test/qunit/unit"
+], function() {
+	"use strict";
+});
